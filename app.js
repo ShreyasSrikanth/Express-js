5c31c819e3dfeb83c022665252d01a8515cc8470
Shreyas Srikanth <sshreyas567@gmail.com>
2023-11-16 03:17:07 +0530

    handling routes for middlewear

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
 
-app.use((req,res,next) =>{
-   console.log("In the middlewear");
-   next();
-})
+// app.use((req,res,next) =>{
+//    console.log("In the middlewear");
+//    next();
+// })
+// app.use('/',(req,res,next) =>{
+//     console.log("This always runs");
+//     next();
+// });
+app.use(bodyParser.urlencoded({extended:false}));
 
-app.use((req,res,next) =>{
-    console.log("In the next middlewear");
-    res.send('<h1>Express JS</h1>')
-})
+app.use('/add-product',(req,res,next) =>{
+    res.send('<form action="/product" method="POST"><input type="number" name="title"><br><input type="text" name="title"><button type ="submit">Add a product</button></form>')
+});
 
+app.post('/product',(req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/')
+});
+
+app.use('/',(req,res,next) =>{
+    res.send('<h1>Hello from Express JS</h1>')
+})
 // const server = http.createServer(app);
 // server.listen(3000);
 
